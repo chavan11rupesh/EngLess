afd4bfd42915edcae708c38e072020e5dda526f0
bhaver11 <bhaveshy11@gmail.com>
2017-08-18 13:40:55 +0530

    added routes in home.clj

--- engless/src/clj/engless/routes/home.clj
+++ engless/src/clj/engless/routes/home.clj
@@ -2,7 +2,8 @@
   (:require [engless.layout :as layout]
             [compojure.core :refer [defroutes GET]]
             [ring.util.http-response :as response]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [engless.view :as view]))
 
 (defn home-page []
   (layout/render "home.html"))
@@ -12,5 +13,10 @@
        (home-page))
   (GET "/docs" []
        (-> (response/ok (-> "docs/docs.md" io/resource slurp))
-           (response/header "Content-Type" "text/plain; charset=utf-8"))))
-
+           (response/header "Content-Type" "text/plain; charset=utf-8")))
+  (GET "/user" [user password]
+       (layout/render-json (view/check-user user password)))
+  (GET "/words" [location]
+       (layout/render-json (view/get-words-at location)))
+  (GET "/word" [level]
+       (layout/render-json (view/get-word-of level))))
